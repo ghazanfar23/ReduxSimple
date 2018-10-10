06d630dee72cc95a3526e6b4a2f3c85bf08bd750
gali4 <gali3@uh.edu>
2018-10-10 16:49:31 -0500

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyCZX9GrNJpPgkA5B6PDu3UA5Gqz5IP0b_s';
+import VideoList from './components/video_list';
+
+
+
 // Create new component
 // produce some html
-const App =  () => {
-    // this html is jsx
-    return <div>HI!</div>;
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('Surfboards');
+
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+            // this.setState({ videos: videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+
 }
 
 // take this components HTML and put it
